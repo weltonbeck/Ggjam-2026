aaf31cb52bf646357e6556e50c0344876fed9a43
Efraim Marcatto <efraimmarcatto@gmail.com>
2026-01-30 20:43:48 -0300

    Refactors bullet collision logic
    Improves bullet collision logic by checking if the
    collided object is not a drone rather than checking if
    the collided object is a player.
    
    Sets collision layers and masks for the bullet.

--- scenes/entities/drone_bullet.gd
+++ scenes/entities/drone_bullet.gd
@@ -6,14 +6,9 @@ var direction: Vector2 = Vector2.RIGHT
 func _process(delta):
 	position += direction * speed * delta
 
-# Destroy bullet when it leaves the screen
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _on_body_entered(body):
-	if body.name == "Player":
-		# Add logic to damage player here (e.g., body.take_damage())
-		queue_free()
-	elif body.name != "Drone": 
-		# Destroy bullet on wall hit, ignore the Drone itself
+	if "drone" not in body.name.to_lower(): 
 		queue_free()
